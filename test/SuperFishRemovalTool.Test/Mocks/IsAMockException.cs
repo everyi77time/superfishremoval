ac229a241111193823a6fc38d21a95e47219ec12
everyi77time <55588166+everyi77time@users.noreply.github.com>
2019-09-25 23:06:21 +0700

    Delete IsAMockException.cs

--- test/SuperFishRemovalTool.Test/Mocks/IsAMockException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace SuperFishRemovalTool.Test.Mocks
-{
-    class IsAMockException : Exception
-    {
-        public IsAMockException() : base("The current mock is not valid for this operation.") { }
-    }
-}
